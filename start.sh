79a8c62e44068b7b37ccacbf895ad66b5af03b4e
yuw2025 <yuwen.cai@shducui.com>
2025-07-24 16:44:33 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@
 # --- 启动 FastAPI ASR 服务 ---
 echo "--- 启动 FastAPI ASR 服务 ---"
 # Render 或 Hugging Face Spaces 会通过 $PORT 环境变量告诉我们应该使用哪个端口
-uvicorn app:app --host 0.0.0.0 --port $PORT
+uvicorn main:app --host 0.0.0.0 --port $PORT
